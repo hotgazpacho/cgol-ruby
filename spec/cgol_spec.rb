241637777289cb102e7d905101124243cc8f6f37
Will Green <will@hotgazpacho.org>
2010-08-16 11:17:42 -0400

    Extracted knowledge of LiveCell/DeadCell into CellGenerator

--- spec/cgol_spec.rb
+++ spec/cgol_spec.rb
@@ -1,13 +1,19 @@
 require 'spec'
 
+class CellGenerator
+  def next_generation(cell)
+    if cell.sustainable? or cell.fertile?
+      LiveCell.new
+    else
+      DeadCell.new
+    end
+  end
+end
+
 module Cell
   attr_reader :neighbors, :state
   def initialize
     @neighbors = []
-  end
-  private
-  def live_neighbor_count
-    @neighbors.select {|c| c.state == :alive}.size
   end
   def fertile?
     live_neighbor_count == 3
@@ -15,6 +21,9 @@ module Cell
   def sustainable?
     [2,3].include? live_neighbor_count
   end
+  def live_neighbor_count
+    @neighbors.select {|c| c.state == :alive}.size
+  end  
 end
 
 class LiveCell
@@ -23,63 +32,57 @@ class LiveCell
     super
     @state = :alive
   end
-  def next_generation
-    sustainable? ? LiveCell.new : DeadCell.new
-  end
 end
 
 class DeadCell
   include Cell
-  def next_generation
-    fertile? ? LiveCell.new : DeadCell.new
-  end
 end
 
-describe LiveCell do
-  describe "next_generation" do
+describe CellGenerator do
+  before :each do
+    @generator = CellGenerator.new
+  end
+  describe "Current cell is a LiveCell" do
     before :each do
       @live_cell = LiveCell.new
     end
     it "produces a DeadCell when it has fewer than 2 LiveCell neighbors" do
       @live_cell.neighbors << LiveCell.new
-      next_gen_cell = @live_cell.next_generation
+      next_gen_cell = @generator.next_generation @live_cell
       next_gen_cell.should be_kind_of DeadCell
     end
     it "produces a DeadCell when it has more than 3 LiveCell neighbors" do
       (1..4).each { @live_cell.neighbors << LiveCell.new }
-      next_gen_cell = @live_cell.next_generation
+      next_gen_cell = @generator.next_generation @live_cell
       next_gen_cell.should be_kind_of DeadCell
     end
     it "produces a LiveCell when it has 2 LiveCell neighbors" do
       (1..2).each { @live_cell.neighbors << LiveCell.new }
-      next_gen_cell = @live_cell.next_generation
+      next_gen_cell = @generator.next_generation @live_cell
       next_gen_cell.should be_kind_of LiveCell
     end
     it "produces a LiveCell when it has 3 LiveCell neighbors" do
       (1..3).each { @live_cell.neighbors << LiveCell.new }
-      next_gen_cell = @live_cell.next_generation
+      next_gen_cell = @generator.next_generation @live_cell
       next_gen_cell.should be_kind_of LiveCell
     end
   end
-end
-
-describe DeadCell do
-  describe "next_generation" do
+  describe "Current cell is a DeadCell" do
     before :each do
       @dead_cell = DeadCell.new
     end
     it "produces a LiveCell when it has 3 LiveCell neighbors" do
       (1..3).each { @dead_cell.neighbors << LiveCell.new }
-      next_gen_cell = @dead_cell.next_generation
+      next_gen_cell = @generator.next_generation @dead_cell
       next_gen_cell.should be_kind_of LiveCell
     end
     it "produces a DeadCell when it has less than 3 LiveCell neighbors" do
-      next_gen_cell = @dead_cell.next_generation
+      next_gen_cell = @generator.next_generation @dead_cell
       next_gen_cell.should be_kind_of DeadCell
     end
     it "produces a DeadCell when it has more than 3 LiveCell neighbors" do
       (1..4).each { @dead_cell.neighbors << LiveCell.new }
-      next_gen_cell = @dead_cell.next_generation
+      next_gen_cell = @generator.next_generation @dead_cell
       next_gen_cell.should be_kind_of DeadCell
     end
   end
