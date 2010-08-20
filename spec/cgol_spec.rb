3114f28d507c8a8b881b8f97d2269f34fbeb6cf0
hotgazpacho <will@hotgazpacho.org>
2010-08-20 00:10:58 -0400

    Add a couple missing tests

--- spec/cgol_spec.rb
+++ spec/cgol_spec.rb
@@ -201,3 +201,17 @@ describe CellGenerator do
     end
   end
 end
+
+describe LiveCell do
+  it "should be alive" do
+    c = LiveCell.new
+    c.alive?.should be_true
+  end
+end
+
+describe DeadCell do
+  it "should not be alive" do
+    c = DeadCell.new
+    c.alive?.should be_false
+  end
+end
